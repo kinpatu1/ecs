eb7828469b2cf20f249a08d59f842079ff8c7f51
miki <kinpatu116@gmail.com>
2023-09-13 04:30:32 +0900

    init

---
+++ service/S3-ECS-RDS/README.md
@@ -0,0 +1,5 @@
+## EC2
+以下コマンド実施
+sudo yum install mysql -y 
+## ECR
+docker fileは適当なものを利用している
--- service/S3-ECS-RDS/main.tf
+++ service/S3-ECS-RDS/main.tf
@@ -1,17 +1,17 @@
-module "rds" {
-  ### Module Path
-  source = "../../modules/rds"
-
-  rds_name           = "${var.project}-rds"
-  security_group_rds = "${var.project}-security_group-rds"
-  vpc_id             = var.vpc_id
-  master_password    = var.master_password
-  database_name      = var.project
-  master_username    = "admin"
-  subnet_group       = "${var.project}-subnet_group"
-  subnet_public-a_id = var.subnet_public-a_id
-  subnet_public-c_id = var.subnet_public-c_id
-}
+# module "rds" {
+#   ### Module Path
+#   source = "../../modules/rds"
+# 
+#   rds_name           = "${var.project}-rds"
+#   security_group_rds = "${var.project}-security_group-rds"
+#   vpc_id             = var.vpc_id
+#   master_password    = var.master_password
+#   database_name      = var.project
+#   master_username    = "admin"
+#   subnet_group       = "${var.project}-subnet_group"
+#   subnet_public-a_id = var.subnet_public-a_id
+#   subnet_public-c_id = var.subnet_public-c_id
+# }
 
 module "s3" {
   ### Module Path
