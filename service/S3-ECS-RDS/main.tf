f6d0562a3a42877ef92d5a5e90b245324452a032
miki <kinpatu116@gmail.com>
2023-09-13 02:10:49 +0900

    init

--- service/S3-ECS-RDS/main.tf
+++ service/S3-ECS-RDS/main.tf
@@ -4,13 +4,13 @@ module "rds" {
 
   rds_name           = "${var.project}-rds"
   security_group_rds = "${var.project}-security_group-rds"
-  vpc_id             = module.vpc.project_vpc_id
+  vpc_id             = var.vpc_id
   master_password    = var.master_password
   database_name      = var.project
   master_username    = "admin"
   subnet_group       = "${var.project}-subnet_group"
-  subnet_public-a_id = module.vpc.project_subnet_public_id_1
-  subnet_public-c_id = module.vpc.project_subnet_public_id_2
+  subnet_public-a_id = var.subnet_public-a_id
+  subnet_public-c_id = var.subnet_public-c_id
 }
 
 module "s3" {
--- service/S3-ECS-RDS/variable.tf
+++ service/S3-ECS-RDS/variable.tf
@@ -7,3 +7,18 @@ variable "project" {
 }
 
 variable "master_password" {}
+
+variable "vpc_id" {
+  type    = string
+  default = "vpc-0f72eaebca3d4cc26"
+}
+
+variable "subnet_public-a_id" {
+  type    = string
+  default = "subnet-075bc52d0608b4468"
+}
+
+variable "subnet_public-c_id" {
+  type    = string
+  default = "subnet-00033c9e52b39f4ac"
+}
