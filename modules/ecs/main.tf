c6996b8091c6900e9a1a890cb82ae91f14499897
miki <kinpatu116@gmail.com>
2023-09-13 04:42:03 +0900

    add ecs

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,3 +1,28 @@
-resource "aws_ecs_task_definition" "service" {
-  family = var.task_definition_name
+resource "aws_ecs_task_definition" "taskdef_service_go" {
+  family                = var.task_definition_name
+  container_definitions = jsonencode(
+[
+        {
+            "name": "miki_container_name",
+            "image": "httpd",
+            "essential": true,
+            "memory": 200,
+            "portMappings": [
+                {
+                    "containerPort": 80,
+                    "protocol": "tcp"
+                }
+            ]
+      }
+])
+
+  requires_compatibilities = ["EC2"]
+  network_mode = "bridge"
+
+  lifecycle {
+    ignore_changes = [
+      container_definitions
+    ]
+  }
 }
+
