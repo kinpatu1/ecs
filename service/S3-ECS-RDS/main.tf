c97365353e7b26562758f6596208567db6df5811
miki <kinpatu116@gmail.com>
2023-09-13 15:22:03 +0900

    add ecs

--- service/S3-ECS-RDS/main.tf
+++ service/S3-ECS-RDS/main.tf
@@ -1,23 +1,23 @@
-# module "rds" {
-#   ### Module Path
-#   source = "../../modules/rds"
-# 
-#   rds_name           = "${var.project}-rds"
-#   security_group_rds = "${var.project}-security_group-rds"
-#   vpc_id             = var.vpc_id
-#   master_password    = var.master_password
-#   database_name      = var.project
-#   master_username    = "admin"
-#   subnet_group       = "${var.project}-subnet_group"
-#   subnet_public-a_id = var.subnet_public-a_id
-#   subnet_public-c_id = var.subnet_public-c_id
-# }
+module "rds" {
+  ### Module Path
+  source = "../../modules/rds"
+
+  rds_name           = "${var.project}-rds"
+  security_group_rds = "${var.project}-security_group-rds"
+  vpc_id             = var.vpc_id
+  master_password    = var.master_password
+  database_name      = var.project
+  master_username    = "admin"
+  subnet_group       = "${var.project}-subnet_group"
+  subnet_public-a_id = var.subnet_public-a_id
+  subnet_public-c_id = var.subnet_public-c_id
+}
 
 module "s3" {
   ### Module Path
   source = "../../modules/s3"
 
-  s3_bucket_name = "${var.project}-s3-202301111"
+  s3_bucket_name = "${var.project}-s3"
 }
 
 module "ecr" {
--- service/S3-ECS-RDS/variable.tf
+++ service/S3-ECS-RDS/variable.tf
@@ -1,28 +1,14 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
-variable "project" {
-  type    = string
-  default = "flow"
-}
+variable "project" {}
 
-variable "account" {
-  default = "074708073377"
-}
+variable "account" {}
 
 variable "master_password" {}
 
-variable "vpc_id" {
-  type    = string
-  default = "vpc-0f72eaebca3d4cc26"
-}
+variable "vpc_id" {}
 
-variable "subnet_public-a_id" {
-  type    = string
-  default = "subnet-075bc52d0608b4468"
-}
+variable "subnet_public-a_id" {}
 
-variable "subnet_public-c_id" {
-  type    = string
-  default = "subnet-00033c9e52b39f4ac"
-}
+variable "subnet_public-c_id" {}
