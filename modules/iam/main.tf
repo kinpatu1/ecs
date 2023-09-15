38383853a32a72199694459eb094f185fe4e9232
miki <kinpatu116@gmail.com>
2023-09-16 08:40:52 +0900

    fix ecs

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -19,3 +19,8 @@ resource "aws_iam_role" "ecs_taskdef" {
     tag-key = var.role_name_ecs_taskdef
   }
 }
+
+resource "aws_iam_role_policy_attachment" "ecs_taskdef" {
+  role       = aws_iam_role.ecs_taskdef.name
+  policy_arn = arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy
+}
