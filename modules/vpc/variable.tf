2a1a9fd9b20015e11a72be20904f76b64efc729f
miki <kinpatu116@gmail.com>
2023-09-16 12:18:05 +0900

    fix iam

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -12,5 +12,5 @@ variable "eip_name" {}
 
 variable "availability_zones" {
   type    = list(string)
-  default = ["ap-northeast-1a", "ap-northeast-1c"]
+  default = ["ap-northeast-1a", "ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
 }
