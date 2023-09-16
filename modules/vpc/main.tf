289f1b6e4f80cf7b180b35ae2c7d5063c90e15d4
miki <kinpatu116@gmail.com>
2023-09-16 10:28:39 +0900

    fix iam

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -101,7 +101,7 @@ resource "aws_route_table" "private_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gateway.id
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
 
   tags = {
